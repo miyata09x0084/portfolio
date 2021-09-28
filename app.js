a0404239bd2d0aa410f1e0557954a33ca1852336
miyata09x0084 <59190800+miyata09x0084@users.noreply.github.com>
2021-09-28 18:33:55 +0900

    use shader

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 import * as THREE from 'three';
 import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls.js';
+import fragment from './shaders/fragment.glsl'
+import vertex from './shaders/vertex.glsl'
 
 export default class Sketch {
     constructor(options) {
@@ -12,7 +14,11 @@ export default class Sketch {
     
         this.scene = new THREE.Scene();
 
-        this.renderer = new THREE.WebGLRenderer( { antialias: true } );
+        this.renderer = new THREE.WebGLRenderer( { 
+            antialias: true ,
+            alpha: true
+        } );
+        this.renderer.setPixelRatio(window.devicePixelRatio);
         this.container.appendChild(this.renderer.domElement);
         this.controls = new OrbitControls(this.camera, this.renderer.domElement);
 
@@ -37,8 +43,17 @@ export default class Sketch {
     }
 
     addObject(){
-        this.geometry = new THREE.BoxGeometry( 0.2, 0.2, 0.2 );
-        this.material = new THREE.MeshNormalMaterial();
+        // this.geometry = new THREE.BoxGeometry( 0.2, 0.2, 0.2 );
+        this.geometry = new THREE.PlaneBufferGeometry( 0.5, 0.5 );
+        // this.material = new THREE.MeshNormalMaterial();
+        this.material = new THREE.ShaderMaterial({
+            uniforms: {
+                time: { value: 1.0 },
+                resolution: { value: new THREE.Vector2() }
+            },
+            vertexShader: vertex,
+            fragmentShader: fragment,
+        });
     
         this.mesh = new THREE.Mesh( this.geometry, this.material );
         this.scene.add( this.mesh );
