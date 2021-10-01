dd884e4ed69e33ad5f1369a10db7c34f03337716
miyata09x0084 <59190800+miyata09x0084@users.noreply.github.com>
2021-10-01 15:18:34 +0900

    add html and css

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ import * as THREE from 'three';
 import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls.js';
 import fragment from './shaders/fragment.glsl'
 import vertex from './shaders/vertex.glsl'
-import testTexture from './static/water.jpg'
+import testTexture from './img/texture.jpg'
+import * as dat from 'dat.gui'
+import gsap from 'gsap'
 
 export default class Sketch {
     constructor(options) {
@@ -10,8 +12,10 @@ export default class Sketch {
         this.width = this.container.offsetWidth;
         this.height = this.container.offsetHeight;
 
-        this.camera = new THREE.PerspectiveCamera( 70, this.width / this.height, 0.01, 10 );
-        this.camera.position.z = 1;
+        this.camera = new THREE.PerspectiveCamera( 80, this.width / this.height, 10, 1000 );
+        this.camera.position.z = 600;
+
+        this.camera.fov = 2*Math.atan( (this.height/2)/600 ) * 180/Math.PI;
     
         this.scene = new THREE.Scene();
 
@@ -24,11 +28,20 @@ export default class Sketch {
         this.controls = new OrbitControls(this.camera, this.renderer.domElement);
 
         this.time = 0;
-        this.resize()
+        this.setupSettings();
+        this.resize();
         this.addObject();
         this.render();
 
-        this.setupResize()
+        this.setupResize();
+    }
+
+    setupSettings(){
+        this.settings = {
+            progress: 0
+        }
+        this.gui = new dat.GUI();
+        this.gui.add(this.settings, "progress", 0, 1, 0.001);
     }
 
     resize(){
@@ -44,25 +57,50 @@ export default class Sketch {
     }
 
     addObject(){
-        this.geometry = new THREE.PlaneBufferGeometry( 0.5, 0.5, 100, 100 );
-        this.geometry = new THREE.SphereBufferGeometry( 0.5, 30, 30 );
+        this.geometry = new THREE.PlaneBufferGeometry( 300, 300, 100, 100 );
         this.material = new THREE.ShaderMaterial({
             uniforms: {
                 time: { value: 1.0 },
+                uProgress: { value: 0 },
                 uTexture: { value: new THREE.TextureLoader().load(testTexture) },
-                resolution: { value: new THREE.Vector2() }
+                uTextureSize: { value: new THREE.Vector2( 100, 100 ) },
+                uCorners: { value: new THREE.Vector4( 0, 0, 0, 0 ) },
+                uResolution: { value: new THREE.Vector2( this.width, this.height ) },
+                uQuadSize: { value: new THREE.Vector2( 300, 300 ) }
             },
             vertexShader: vertex,
             fragmentShader: fragment,
         });
+
+        this.tl = gsap.timeline()
+            .to(this.material.uniforms.uCorners.value, {
+                x:1,
+                duration: 1
+            })
+            .to(this.material.uniforms.uCorners.value, {
+                y:1,
+                duration: 1
+            }, 0.2)
+            .to(this.material.uniforms.uCorners.value, {
+                z:1,
+                duration: 1
+            }, 0.4)
+            .to(this.material.uniforms.uCorners.value, {
+                w:1,
+                duration: 1
+            }, 0.6)
     
         this.mesh = new THREE.Mesh( this.geometry, this.material );
         this.scene.add( this.mesh );
+        this.mesh.position.x = 300;
+        // this.mesh.rotation.z = 0.5;
     }
 
     render(){
         this.time += 0.05;
         this.material.uniforms.time.value = this.time;
+        this.material.uniforms.uProgress.value = this.settings.progress;
+        // this.tl.progress(this.settings.progress)
         this.mesh.rotation.x = this.time / 2000;
         this.mesh.rotation.y = this.time / 1000;
 
--- shaders/vertex.glsl
+++ shaders/vertex.glsl
@@ -1,14 +1,30 @@
 uniform float time;
-varying float pulse;
+uniform float uProgress;
+uniform vec2 uResolution;
+uniform vec4 uCorners;
+uniform vec2 uQuadSize;
 
 varying vec2 vUv;
-varying vec3 vNormal;
+varying vec2 vSize;
 
 void main() {
+    float PI = 3.1415926;
     vUv = uv;
-    vNormal = normal;
-    vec3 newPosition = position;
-    newPosition.z = 0.05*sin(length(position)*30.+time);
-    pulse = 20.*newPosition.z;
-    gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
+    float sine = sin(PI*uProgress);
+    float waves = sine*0.1*sin(5.*length(uv) + 13.*uProgress);
+    vec4 defaultState = modelMatrix*vec4( position, 1.0 );
+    vec4 fullScreenState = vec4( position, 1.0 );
+    fullScreenState.x *= uResolution.x/uQuadSize.x;
+    fullScreenState.y *= uResolution.y/uQuadSize.y;
+    float cornersProgress = mix(
+        mix(uCorners.x, uCorners.y, uv.x),
+        mix(uCorners.z, uCorners.w, uv.x),
+        uv.y
+    );
+
+    vec4 finalState = mix(defaultState,fullScreenState,uProgress + waves);
+
+    vSize = mix(uQuadSize, uResolution, uProgress);
+
+    gl_Position = projectionMatrix * viewMatrix * finalState;
 }
